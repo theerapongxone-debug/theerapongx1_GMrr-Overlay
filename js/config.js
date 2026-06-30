4d3a4646d10ee9914f80814718db7b82eca67b33
theerapongxone-debug <theerapongxone@gmail.com>
2026-06-30 09:13:51 +0700

    Add configuration for streamer and social media links

--- js/config.js
+++ js/config.js
@@ -1,1 +1,20 @@
+const CONFIG = {
 
+streamer:{
+    name:"Blue Panther X1",
+    subtitle:"Gaming Stream"
+},
+
+youtube:"@YOUNG.FirstX1",
+
+twitch:"theerapongx1_GMrr",
+
+facebook:"Blue Panther X1",
+
+donateQR:"assets/qrcode.png",
+
+logo:"assets/logo.png",
+
+avatar:"assets/avatar.png"
+
+};
